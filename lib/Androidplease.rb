2b39eef120d817d2a96914e3a0eadba6d89d27b7
cesar ferreira <cesar.manuel.ferreira@gmail.com>
2015-03-25 00:13:05 +0000

    Added suggestions

--- lib/Androidplease.rb
+++ lib/Androidplease.rb
@@ -25,61 +25,81 @@ module AndroidPlease
       if response.code == '200'
         result = JSON.parse(response.body)
 
+        # Amount of results
         num_results = result['response']['numFound']
 
-        result_list = Array.new #=> []
+        suggestions = nil
 
-
-        result['response']['docs'].each do |doc|
-          output = "compile '#{doc['id']}:#{doc['latestVersion']}'"
-          result_list.push(output)
+        # Possible suggestions
+        if num_results==0
+          suggestions = result['spellcheck']['suggestions'].last['suggestion']
         end
 
-        return num_results, result_list
+        # Init the result list
+        result_list = Array.new
+
+        # Iterate the libraries
+        result['response']['docs'].each do |doc|
+          output = "compile '#{doc['id']}:#{doc['latestVersion']}'"
+          result_list << output
+        end
+
+        return num_results, result_list, suggestions
       else
-        puts 'ERROR!!!'.red
+        puts 'Unknown ERROR!!!\n(are you even connected to the internet brah?)'.red
       end
     end
 
     def self.query_offline_reps(query)
+      #
+      #  compile 'com.android.support:appcompat-v7:21.0.3'
+      #  compile 'com.android.support:cardview-v7:21.0.3'
+      #  compile 'com.android.support:gridlayout-v7:21.0.3'
+      #  compile 'com.android.support:leanback-v17:21.0.3'
+      #  compile 'com.android.support:mediarouter-v7:21.0.3'
+      #  compile 'com.android.support:palette-v7:21.0.3'
+      #  compile 'com.android.support:recyclerview-v7:21.0.3'
+      #  compile 'com.android.support:support-annotations:21.0.3'
+      #  compile 'com.android.support:support-v13:21.0.3'
+      #  compile 'com.android.support:support-v4:21.0.3'
 
+      #com.google.android.gms:play-services-wearable:6.5.87
+      #com.google.android.gms:play-services:6.5.87
     end
 
 
     def self.initialize (param)
 
-      puts param?.lenght
+      # puts param?.lenght
 
       if param == nil
-        puts 'Missing query parameter'.red
+        puts "Missing query parameter\n\tusage: AndroidPlease SEARCH_QUERY\n".red
       else
-        num_results, result_list = fetch_results(param)
+        if param.size>=3
+          num_results, result_list, suggestions = fetch_results(param)
 
-        puts "Found: #{num_results} results for '#{param}'"
+          puts "Found: #{num_results} results for '#{param}'"
 
-        result_list.each do |result|
-          puts result.green
+          result_list.each do |result|
+            puts result.green
+          end
+
+          # handle the suggestions
+          if suggestions!=nil
+
+            output = ''
+
+            # iterate the suggestions
+            suggestions.each do |suggestion|
+              output += "#{suggestion} ".yellow
+            end
+            puts "Did you mean: #{output}"
+          end
+        else
+          puts 'The search needs 3+ characters'.red
+
         end
-
-        # se o resultado for 0 mostrar as sugestoes
-        #Did you mean: quick quickr quickvisu quickhull quickgeo ?
-        #
-        #  compile 'com.android.support:appcompat-v7:21.0.3'
-        #  compile 'com.android.support:cardview-v7:21.0.3'
-        #  compile 'com.android.support:gridlayout-v7:21.0.3'
-        #  compile 'com.android.support:leanback-v17:21.0.3'
-        #  compile 'com.android.support:mediarouter-v7:21.0.3'
-        #  compile 'com.android.support:palette-v7:21.0.3'
-        #  compile 'com.android.support:recyclerview-v7:21.0.3'
-        #  compile 'com.android.support:support-annotations:21.0.3'
-        #  compile 'com.android.support:support-v13:21.0.3'
-        #  compile 'com.android.support:support-v4:21.0.3'
-
-        #com.google.android.gms:play-services-wearable:6.5.87
-        #com.google.android.gms:play-services:6.5.87
-
       end
-
     end
   end
 end
