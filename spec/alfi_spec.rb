fedadd751b5a873ee4b78f0ccb918e707e809235
luispereira <luispereira268@gmail.com>
2016-05-17 10:11:41 +0100

    % Changing tests

--- spec/alfi_spec.rb
+++ spec/alfi_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Alfi do
   it 'Test the query_url' do
-    str = Alfi::Providers::Maven.new('picasso').query_url('picasso')
+    str = Alfi::Providers::Maven.new('picasso', Array.new).query_url('picasso')
     expect(str).to eq 'http://search.maven.org/solrsearch/select?q=picasso&rows=350&wt=json'
   end
 
   context 'suggestions' do
     it 'should return 0 suggestions' do
       VCR.use_cassette('search_active_android_with_no_suggestions') do
-        maven_provider = Alfi::Providers::Maven.new('active-android')
+        maven_provider = Alfi::Providers::Maven.new('active-android', Array.new)
 
         expect(maven_provider).to_not receive(:add_suggestions)
         maven_provider.call
@@ -18,7 +18,7 @@ describe Alfi do
 
     it 'should return suggestions' do
       VCR.use_cassette('search_picassoa_with_suggestions') do
-        maven_provider = Alfi::Providers::Maven.new('picassoa')
+        maven_provider = Alfi::Providers::Maven.new('picassoa', Array.new)
 
         expect(maven_provider).to receive(:add_suggestions).with(array_including('picasso', 'piccolo'))
         maven_provider.call
