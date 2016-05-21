8ea7620615aa39b213cdb07e0d880861e43c892c
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2016-05-22 00:02:14 +0100

    fixed linux bug

--- lib/alfi.rb
+++ lib/alfi.rb
@@ -6,5 +6,6 @@ require 'net/http'
 require 'uri'
 require 'colorize'
 require 'alfi/providers'
+require 'alfi/providers/base'
 
 Dir["#{File.dirname(__FILE__)}/alfi/**/*.rb"].each { |file| require file }
